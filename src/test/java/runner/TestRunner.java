af14d3b508998b70f7f3e31d84a2ba7d489becc9
husheng-kooboo <husheng@kooboo.cn>
2019-08-22 01:22:00 -0700

    Update

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,13 +1,18 @@
 package runner;
 
+import io.cucumber.junit.Cucumber;
+//import io.cucumber.junit.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.junit.runner.RunWith;
 
 //@RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/java/Features/login.feature"},
         glue = {"Steps"},
-        tags = {"@login"}
+        tags = {"@login"},
+        monochrome = true,
+        plugin = {"json:target/cucumber.json","html:target/site/cucumber-pretty"}
 )
-public class TestRunner extends AbstractTestNGCucumberTests {
+public class TestRunner extends  AbstractTestNGCucumberTests{
 }
